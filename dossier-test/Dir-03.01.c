f839bd37e162764df2ffa706b56aa7a94bf7274d
Morgan Billon <morgan.billon@vector.com>
2023-03-31 15:57:22 +0200

    new file in new directory

---
+++ dossier-test/Dir-03.01.c
@@ -0,0 +1,21 @@
+/* >>>------------------------------------------------------------
+ * 
+ * File: Dir-03.01.c,  Module: M3CM-1.0.3-QAC-8.2.2
+ * 
+ * RULE Dir-3.1 (Required):
+ * All code shall be traceable to documented requirements
+ * 
+ * Not enforced
+ * 
+ * <<<------------------------------------------------------------ */
+
+
+
+
+#include "misra.h"
+#include "m3cmex.h"
+
+extern int16_t dir_0301( void )
+{
+   return 1;
+}
